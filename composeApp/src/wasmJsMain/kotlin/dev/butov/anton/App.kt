3c54abd1f2ed6cb1715c6d9ab43193928cbbc451
Anton Butov <butov6101@gmail.com>
2025-06-28 13:18:47 +0300

    VCSOB-123 refactor(wasmJsMain): extract background logic to separate function and update styles
    Extracted background rendering logic from App.kt to new BackGround composable. Updated scrollbar and divider colors in Colors.kt and styles.css for better visibility.

--- composeApp/src/wasmJsMain/kotlin/dev/butov/anton/App.kt
+++ composeApp/src/wasmJsMain/kotlin/dev/butov/anton/App.kt
@@ -1,23 +1,20 @@
 package dev.butov.anton
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 
 import antonbutov.composeapp.generated.resources.Res
-import antonbutov.composeapp.generated.resources.compose_multiplatform
 import antonbutov.composeapp.generated.resources.redBack
 
 @Composable
@@ -50,43 +47,56 @@ fun App() {
 
              */
 
-            LazyColumn(
-                state = scrollState,
-            ) {
-                item {
-                    Box(
-                        Modifier
-                            .background(Colors.background)
-                            .height(3500.dp)
-                            .fillMaxWidth()
-                    ) {
-                        Row(
-                            Modifier
-                                .fillMaxSize()
-                                .padding(horizontal = 80.dp)
-                        ) {
-                            VerticalDivider(color = Colors.onBackground)
-                            Spacer(Modifier.weight(1f))
-                            VerticalDivider(color = Colors.onBackground)
-                            Spacer(Modifier.weight(1f))
-                            VerticalDivider(color = Colors.onBackground)
-                            Spacer(Modifier.weight(1f))
-                            VerticalDivider(color = Colors.onBackground)
-                            Spacer(Modifier.weight(1f))
-                            VerticalDivider(color = Colors.onBackground)
-                        }
-                        Image(
-                            modifier = Modifier.fillMaxWidth().padding(horizontal = 80.dp, vertical = 550.dp),
-                            painter = painterResource(Res.drawable.redBack), contentDescription = null,
-                            contentScale = ContentScale.FillBounds
-                        )
-                    }
-                }
-            }
+            BackGround(scrollState)
+            MainColumn()
             VerticalScrollbar(
                 adapter = rememberScrollbarAdapter(scrollState),
                 modifier = Modifier.fillMaxHeight().align(Alignment.CenterEnd)
             )
+        }
+    }
+}
+
+@Composable
+fun MainColumn() {
+    Column {
+        
+    }
+}
+
+@Composable
+fun BackGround(scrollState: LazyListState) {
+    LazyColumn(
+        state = scrollState,
+    ) {
+        item {
+            Box(
+                Modifier
+                    .background(Colors.background)
+                    .height(3500.dp)
+                    .fillMaxWidth()
+            ) {
+                Row(
+                    Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 80.dp)
+                ) {
+                    VerticalDivider(color = Colors.onBackground)
+                    Spacer(Modifier.weight(1f))
+                    VerticalDivider(color = Colors.onBackground)
+                    Spacer(Modifier.weight(1f))
+                    VerticalDivider(color = Colors.onBackground)
+                    Spacer(Modifier.weight(1f))
+                    VerticalDivider(color = Colors.onBackground)
+                    Spacer(Modifier.weight(1f))
+                    VerticalDivider(color = Colors.onBackground)
+                }
+                Image(
+                    modifier = Modifier.fillMaxWidth().padding(horizontal = 80.dp, vertical = 550.dp),
+                    painter = painterResource(Res.drawable.redBack), contentDescription = null,
+                    contentScale = ContentScale.FillBounds
+                )
+            }
         }
     }
 }
--- composeApp/src/wasmJsMain/kotlin/dev/butov/anton/Colors.kt
+++ composeApp/src/wasmJsMain/kotlin/dev/butov/anton/Colors.kt
@@ -5,5 +5,5 @@ import androidx.compose.ui.graphics.Color
 object Colors {
     val primary = Color(0xE20100D1)
     val background = Color(0xFF0B0B0B)
-    val onBackground = Color(0xFFFFFFFF)
+    val onBackground = Color(0x0AFFFFFF)
 }

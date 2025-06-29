8c97931beb19b730ccaa94f513af1d01d6210943
Anton Butov <butov6101@gmail.com>
2025-06-29 22:08:12 +0300

    VCSOB-123 feat(uikit): add Technology composable and refactor TechnologyHeader
    Added new Technology composable with BoxScope content and refactored TechnologyHeader to use it. Also added support for icon, technology name, and experience parameters.

--- composeApp/src/wasmJsMain/kotlin/dev/butov/anton/uikit/Technology.kt
+++ composeApp/src/wasmJsMain/kotlin/dev/butov/anton/uikit/Technology.kt
@@ -1,6 +1,7 @@
 package dev.butov.anton.uikit
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
@@ -8,21 +9,44 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 
 val TECHNOLOGY_HEIGHT = 110.dp
 val TECHNOLOGY_WIDTH = 155.dp
 
 @Composable
+fun Technology(
+    modifier: Modifier = Modifier,
+    content: @Composable BoxScope.() -> Unit
+) {
+    Box(modifier.height(TECHNOLOGY_HEIGHT).width(TECHNOLOGY_WIDTH)) {
+        content()
+    }
+}
+
+@Composable
 fun TechnologyHeader(
     modifier: Modifier = Modifier
 ) {
-    Box(modifier.height(TECHNOLOGY_HEIGHT).width(TECHNOLOGY_WIDTH)) {
+    Technology {
         Text(
-            modifier = Modifier.align(Alignment.TopStart),
+            modifier = modifier.align(Alignment.TopStart),
             text = "Technologies:",
             style = MaterialTheme.typography.bodyMedium,
         )
     }
 }
+
+@Composable
+fun Technology(
+    modifier: Modifier = Modifier,
+    icon: ImageVector,
+    technology: String,
+    experience: String
+) {
+    Technology {
+
+    }
+}
+

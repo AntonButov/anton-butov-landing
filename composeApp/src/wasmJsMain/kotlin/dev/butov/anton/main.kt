02d5ef184f92bf8dab2d0c9477e37bf62b8d0b8c
AntonButov <58133570+AntonButov@users.noreply.github.com>
2025-07-06 23:14:35 +0300

    Revert "Fix viewport resizing"

--- composeApp/src/wasmJsMain/kotlin/dev/butov/anton/main.kt
+++ composeApp/src/wasmJsMain/kotlin/dev/butov/anton/main.kt
@@ -3,15 +3,10 @@ package dev.butov.anton
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
-import kotlinx.browser.window
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
-    fun render() {
-        ComposeViewport(document.body!!) {
-            App()
-        }
+    ComposeViewport(document.body!!) {
+        App()
     }
-    render()
-    window.onresize = { render() }
 }

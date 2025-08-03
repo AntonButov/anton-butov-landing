85915960b061cab15d725582ad1c838c6bd8f842
AntonButov <58133570+AntonButov@users.noreply.github.com>
2025-08-03 10:10:24 +0400

    VCSMOB-contact-me fix(sendMessage): center Wrong component in BoxScope

--- composeApp/src/wasmJsMain/kotlin/dev/butov/anton/subscreens/sendMessage/Wrong.kt
+++ composeApp/src/wasmJsMain/kotlin/dev/butov/anton/subscreens/sendMessage/Wrong.kt
@@ -1,15 +1,19 @@
 package dev.butov.anton.subscreens.sendMessage
 
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import dev.butov.anton.Colors
 
 @Composable
-fun Wrong(message: String) {
-    Column {
+fun BoxScope.Wrong(message: String) {
+    Column(
+        Modifier.align(Alignment.Center)) {
         Text(
             text = "Some problem:",
             style = MaterialTheme.typography.displayMedium.copy(fontWeight = FontWeight.Medium),

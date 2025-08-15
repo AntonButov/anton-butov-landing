445225fd211d3e463b8c3170453922ff95c5ee58
Anton Butov <butov6101@gmail.com>
2025-08-15 20:39:12 +0300

    about - feat(subscreens/Smirnov.kt): update Smirnov composable layout
    Added current year display and AntonIcons.Ss, removed unused Spacer, adjusted alignment.

--- composeApp/src/wasmJsMain/kotlin/dev/butov/anton/subscreens/Smirnov.kt
+++ composeApp/src/wasmJsMain/kotlin/dev/butov/anton/subscreens/Smirnov.kt
@@ -8,26 +8,33 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import antonbutov.composeapp.generated.resources.Res
 import antonbutov.composeapp.generated.resources.copyright
+import dev.butov.anton.currentYear
+import dev.butov.anton.myiconpack.AntonIcons
+import dev.butov.anton.myiconpack.Ss
 import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun Smirnov() {
-    Row {
+    Row(verticalAlignment = Alignment.CenterVertically) {
         Icon(
             modifier = Modifier.size(20.dp),
             painter = painterResource(Res.drawable.copyright),
             contentDescription = "Smirnov",
         )
-        Spacer(Modifier.size(10.dp))
+        // Spacer(Modifier.size(10.dp))
         Text(
-            text = "Smirnov",
-            fontWeight = FontWeight.Bold,
-            color = MaterialTheme.colorScheme.onSurface,
+            text = currentYear.toString(),
+            style = MaterialTheme.typography.bodyLarge,
+        )
+        Spacer(Modifier.weight(1f))
+        Icon(
+            imageVector = AntonIcons.Ss,
+            contentDescription = "Smirnov",
         )
     }
 }

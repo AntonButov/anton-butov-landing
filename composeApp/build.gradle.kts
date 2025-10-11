60e4446a91515d270da8042a62fb4ef8694ea349
Anton Butov <butov6101@gmail.com>
2025-10-11 16:37:24 +0300

    Revert "fix: Correct build.gradle.kts syntax errors"
    This reverts commit 46aad515ee743bb685e92d9aaa636895ff3e3134.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -36,17 +36,15 @@ tasks.named("check") {
 }
 
 kotlin {
-    @OptIn(ExperimentalWasmDsl::class)
+    @OptIn(org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl::class)
     wasmJs {
-        outputModuleName = "app"
+        binaries.executable("app") // имя без хэша
         browser {
             val rootDirPath = project.rootDir.path
             val projectDirPath = project.projectDir.path
             commonWebpackConfig {
-                outputFileName = "app.mjs"
-                cssSupport {
-                    enabled.set(false)
-                }
+                outputFileName = "app.mjs" // убирает hash для .mjs
+                cssSupport.enabled = false
                 devServer =
                     (devServer ?: KotlinWebpackConfig.DevServer()).apply {
                         static =
@@ -58,7 +56,6 @@ kotlin {
                     }
             }
         }
-        binaries.executable()
     }
 
     sourceSets {
